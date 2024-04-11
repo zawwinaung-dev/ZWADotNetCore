3005c50f0ecf79b6c1a1d94c6e7c3f9bef49a2b5
Zaw Win Aung <itszawaung@gmail.com>
2024-04-11 21:55:50 +0630

    [Complete] Ado.Net CRUD

--- ZWADotNetCore.ConsoleApp/Program.cs
+++ ZWADotNetCore.ConsoleApp/Program.cs
@@ -1,41 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
 using System.Data.SqlClient;
+using ZWADotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "DESKTOP-775DARV"; //server name
-stringBuilder.InitialCatalog = "ZWADotNetCore"; //database name
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "sa@123";
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+//stringBuilder.DataSource = "DESKTOP-775DARV"; //server name
+//stringBuilder.InitialCatalog = "ZWADotNetCore"; //database name
+//stringBuilder.UserID = "sa";
+//stringBuilder.Password = "sa@123";
+//SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
 
-connection.Open();
-Console.WriteLine("Connection open.");
+//connection.Open();
+//Console.WriteLine("Connection open.");
 
-string query = "select * from tbl_blog";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
+//string query = "select * from tbl_blog";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//sqlDataAdapter.Fill(dt);
 
-connection.Close();
-Console.WriteLine("Connection close.");
+//connection.Close();
+//Console.WriteLine("Connection close.");
 
-// dataset => datatable
-// datatable => datarow
-// datarow => datacolumn
+//// dataset => datatable
+//// datatable => datarow
+//// datarow => datacolumn
 
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine("blog id => " + dr["blogid"]);
-    Console.WriteLine("blog title => " + dr["blogtitle"]);
-    Console.WriteLine("blog author => " + dr["blogauthor"]);
-    Console.WriteLine("blog content => " + dr["blogcontent"]);
-    Console.WriteLine("---------------------------------------");
-}
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine("blog id => " + dr["blogid"]);
+//    Console.WriteLine("blog title => " + dr["blogtitle"]);
+//    Console.WriteLine("blog author => " + dr["blogauthor"]);
+//    Console.WriteLine("blog content => " + dr["blogcontent"]);
+//    Console.WriteLine("---------------------------------------");
+//}
 
 // Ado.Net Read
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(12, "test title", "test author", "test content");
+//adoDotNetExample.Delete(12);
+adoDotNetExample.Edit(12);
+adoDotNetExample.Edit(1);
 
 Console.ReadKey();
