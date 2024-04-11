068fb7a2a7288fdfa1f336e46e9f13af42802af6
Zaw Win Aung <itszawaung@gmail.com>
2024-04-11 20:56:58 +0630

    Ado.Net Read

---
+++ README.md
@@ -0,0 +1,7 @@
+UI + Business Logic + Data Access + Db
+
+C# => Db
+
+ZWADotNetCore
+> ZWADotNetCore.ConsoleApp
+> ZWADotNetCore.RestApi
--- ZWADotNetCore.ConsoleApp/Program.cs
+++ ZWADotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DESKTOP-775DARV"; //server name
+stringBuilder.InitialCatalog = "ZWADotNetCore"; //database name
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+connection.Open();
+Console.WriteLine("Connection open.");
+
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection close.");
+
+// dataset => datatable
+// datatable => datarow
+// datarow => datacolumn
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("blog id => " + dr["blogid"]);
+    Console.WriteLine("blog title => " + dr["blogtitle"]);
+    Console.WriteLine("blog author => " + dr["blogauthor"]);
+    Console.WriteLine("blog content => " + dr["blogcontent"]);
+    Console.WriteLine("---------------------------------------");
+}
+
+// Ado.Net Read
+
+Console.ReadKey();
